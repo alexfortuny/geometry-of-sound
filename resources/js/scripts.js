cb8075023df9051dd6d0c0ceb42922c9151b2246
Alexandre Fortuny Villa <alexfortuny@Mac-mini.local>
2020-05-24 00:01:02 +0200

    update

---
+++ resources/js/scripts.js
@@ -0,0 +1,47 @@
+var mousePressed = false;
+/*
+var lastX, lastY;
+var ctx;
+
+function InitThis() {
+    ctx = document.getElementById('circleCanvas').getContext("2d");
+
+    $('.circleCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('.circleCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('.circleCanvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+	    $('.circleCanvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+*/
