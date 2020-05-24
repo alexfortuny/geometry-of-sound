5949011ff2aee80d2715627ec2346f60ad0c931b
Alexandre Fortuny Villa <alexfortuny@Mac-mini.local>
2020-05-24 17:51:38 +0200

    id #position + i created for each point / wrapper margin-bottom: 60px

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,20 @@
 jQuery(document).ready(function($) {
     const points = $('.point');
 
-    points.hover(function() {
-        $(this).css({
-            "background-color": "rgb(255, 151, 151)",
-        });
-    }, function () {
-        $(this).css({
-            "background-color": "rgb(102, 102, 102)",
-        });
-    });
-
-
+    // points.hover(function() {
+    //     $(this).css({
+    //         "background-color": "rgb(255, 151, 151)",
+    //     });
+    // }, function () {
+    //     $(this).css({
+    //         "background-color": "rgb(102, 102, 102)",
+    //     });
+    // });
 
     const canvas = SVG("svg");
 
     for (let i = 0; i < 12; i++) {
-        canvas.circle(10).addClass("pitch").move(300, 0).rotate(30*i, 305, 305);
+        canvas.circle(10).addClass("pitch").attr('id', 'position' + i).move(300, 0).rotate(30*i, 305, 305);
     }
 
     $(".pitch").hover(function() {
@@ -29,8 +27,4 @@ jQuery(document).ready(function($) {
             "fill": "green",
         });
     });
-
-
-
-
 });
