1396b9e64077c83ee2ad5707c55cf61e27d8bf90
Alexandre Fortuny Villa <alexfortuny@Mac-mini.local>
2020-05-25 06:35:52 +0200

    Reorder

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
-
 jQuery(document).ready(function($) {
-    const points = $('.point');
+  const points = $('.point');
 
-    // points.hover(function() {
-    //     $(this).css({
-    //         "background-color": "rgb(255, 151, 151)",
-    //     });
-    // }, function () {
-    //     $(this).css({
-    //         "background-color": "rgb(102, 102, 102)",
-    //     });
-    // });
+  // points.hover(function() {
+  //     $(this).css({
+  //         "background-color": "rgb(255, 151, 151)",
+  //     });
+  // }, function () {
+  //     $(this).css({
+  //         "background-color": "rgb(102, 102, 102)",
+  //     });
+  // });
 
-    const canvas = SVG("svg");
+  const canvas = SVG("svg");
 
-    for (let i = 0; i < 12; i++) {
-        canvas.circle(10).addClass("pitch").attr('id', 'position' + i).move(300, 0).rotate(30*i, 305, 305);
-    }
+  for (let i = 0; i < 12; i++) {
+    canvas.circle(10).addClass("pitch").attr('id', 'position' + i).move(300, 0).rotate(30 * i, 305, 305);
+  }
 
-    $(".pitch").hover(function() {
-        $(this).css({
-            "fill": "blue",
-        });
-    }, function () {
-        $(this).css({
-            "fill": "green",
-        });
+  $(".pitch").hover(function() {
+    $(this).css({
+      "fill": "blue",
     });
+  }, function() {
+    $(this).css({
+      "fill": "green",
+    });
+  });
 });
