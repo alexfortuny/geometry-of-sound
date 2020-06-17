db49543d7b9c5abf41c77b1b727c59c893e097d8
Alexandre Fortuny Villa <alexfortuny@Mac-mini-de-Alexandre.local>
2020-06-17 17:26:06 +0200

    Improving sounds

--- js/script.js
+++ js/script.js
@@ -54,11 +54,11 @@ jQuery(document).ready(function($) {
       });
       SVG(svg).find('circle').attr('clicked', null);
       lastcoord = null;
-         synth.triggerAttackRelease(storedChord.slice(0, -1), '2n');
+         piano.triggerAttackRelease(storedChord.slice(0, -1), '2n');
          storedChord = [];
     } else {
       node.setAttribute('clicked', true);
-      synth.triggerAttackRelease(note, '8n');
+      piano.triggerAttackRelease(note, '8n');
     }
 
 
@@ -81,14 +81,43 @@ jQuery(document).ready(function($) {
   });
 
   //a polysynth composed of 6 Voices of Synth
-  const synth = new Tone.PolySynth(4, Tone.Synth, {
-    oscillator: {
-      type: "sine"
-    }
-  }).toMaster();
+  const piano = new Tone.Sampler({
+			"A0" : "A0.[mp3|ogg]",
+			"C1" : "C1.[mp3|ogg]",
+			"D#1" : "Ds1.[mp3|ogg]",
+			"F#1" : "Fs1.[mp3|ogg]",
+			"A1" : "A1.[mp3|ogg]",
+			"C2" : "C2.[mp3|ogg]",
+			"D#2" : "Ds2.[mp3|ogg]",
+			"F#2" : "Fs2.[mp3|ogg]",
+			"A2" : "A2.[mp3|ogg]",
+			"C3" : "C3.[mp3|ogg]",
+			"D#3" : "Ds3.[mp3|ogg]",
+			"F#3" : "Fs3.[mp3|ogg]",
+			"A3" : "A3.[mp3|ogg]",
+			"C4" : "C4.[mp3|ogg]",
+			"D#4" : "Ds4.[mp3|ogg]",
+			"F#4" : "Fs4.[mp3|ogg]",
+			"A4" : "A4.[mp3|ogg]",
+			"C5" : "C5.[mp3|ogg]",
+			"D#5" : "Ds5.[mp3|ogg]",
+			"F#5" : "Fs5.[mp3|ogg]",
+			"A5" : "A5.[mp3|ogg]",
+			"C6" : "C6.[mp3|ogg]",
+			"D#6" : "Ds6.[mp3|ogg]",
+			"F#6" : "Fs6.[mp3|ogg]",
+			"A6" : "A6.[mp3|ogg]",
+			"C7" : "C7.[mp3|ogg]",
+			"D#7" : "Ds7.[mp3|ogg]",
+			"F#7" : "Fs7.[mp3|ogg]",
+			"A7" : "A7.[mp3|ogg]",
+			"C8" : "C8.[mp3|ogg]"
+		}, {
+			"release" : 1,
+			"baseUrl" : "audio/"
+	}).toMaster();
 
-
-
+  //StaffConfig
   const VF = Vex.Flow;
 
   const vf = new VF.Factory({
@@ -120,9 +149,4 @@ jQuery(document).ready(function($) {
 
 
   }
-
-
-
-
-
 });
